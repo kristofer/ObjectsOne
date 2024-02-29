f619168865c9dc6ec20f91ccf9eae9b0815d5788
Angelinamathew <angelinamathew707@gmail.com>
2024-02-29 14:46:01 -0500

    Finished

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -6,5 +6,32 @@ public class Main {
 
     public static void main(String... args) {
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This is a module-using Hello World!");
+
+        //Person
+        Person person1 = new Person("Jane", 23, 189);
+        System.out.println("Name: "+ person1.name);
+        System.out.println("Age: " + person1.age);
+        System.out.println("Height: " + person1.height);
+        System.out.println();
+
+        //Car
+        Car car = new Car("Honda","CR-V", 2022,29 );
+        System.out.println(car);
+        System.out.println();
+
+        //Chair
+        Chair chair = new Chair("Wood", "brown", 4);
+        System.out.println(chair);
+        System.out.println();
+        //Dog
+        Dog dog = new Dog("Jacky", 3, "German Shepherd" );
+        System.out.println(dog);
+
+
+
+
+    }
+
+
 }
